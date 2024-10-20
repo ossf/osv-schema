31dce8b3624f545c757bf9e82d7f58646b246f87
Gareth Jones <jones258@gmail.com>
2024-10-21 11:14:27 +1300

    feat: support marshalling and unmarshalling `ecosystem.Parsed` to and from a string in JSON
    Signed-off-by: Gareth Jones <jones258@gmail.com>

--- packages/go/ecosystem/ecosystem.go
+++ packages/go/ecosystem/ecosystem.go
@@ -11,6 +11,27 @@ type Parsed struct {
 	Suffix    string
 }
 
+// UnmarshalJSON handles unmarshalls a JSON string into a Parsed struct.
+//
+// This method implements the json.Unmarshaler interface.
+//
+//goland:noinspection GoMixedReceiverTypes
+func (p *Parsed) UnmarshalJSON(data []byte) error {
+	*p = Parse(strings.Trim(string(data), "\""))
+
+	return nil
+}
+
+// MarshalJSON handles marshals a Parsed struct into a JSON string.
+//
+// This method implements the json.Marshaler interface.
+//
+//goland:noinspection GoMixedReceiverTypes
+func (p Parsed) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + p.String() + `"`), nil
+}
+
+//goland:noinspection GoMixedReceiverTypes
 func (p *Parsed) String() string {
 	str := string(p.Ecosystem)
 
