486019f46984a24594c0bb0c618190714777a55b
Gareth Jones <jones258@gmail.com>
2024-10-22 07:39:36 +1300

    feat: support updating markdown table in schema file
    Signed-off-by: Gareth Jones <jones258@gmail.com>

--- scripts/update-ecosystems-lists.py
+++ scripts/update-ecosystems-lists.py
@@ -2,6 +2,9 @@
 
 import json
 
+MARKDOWN_TABLE_MARKER_START = '<!-- begin auto-generated ecosystems list -->'
+MARKDOWN_TABLE_MARKER_END = '<!-- end auto-generated ecosystems list -->'
+
 ecosystems: dict[str, str] = json.loads(open('ecosystems.json').read())
 
 
@@ -28,4 +31,45 @@ def update_json_schema():
   open('validation/schema.json', 'w').write(json.dumps(schema, indent=2) + '\n')
 
 
+def generate_ecosystems_markdown_table() -> str:
+  """
+  Generates a Markdown table of supported ecosystems with descriptions
+  :return:
+  """
+  table = '| Ecosystem | Description |\n'
+  table += '|-----------|-------------|\n'
+
+  for name, description in ecosystems.items():
+    table += f'| `{name}` | {description} |\n'
+
+  return table
+
+
+def update_schema_md():
+  """
+  Updates the schema.md file with the list of ecosystems
+  :return:
+  """
+  md = open('docs/schema.md').read()
+
+  table_start_index = md.index(MARKDOWN_TABLE_MARKER_START)
+  table_end_index = md.index(MARKDOWN_TABLE_MARKER_END)
+
+  assert table_start_index < table_end_index, "Table start index must be before table end index"
+
+  table = generate_ecosystems_markdown_table()
+  table += '| Your ecosystem here. | [Send us a PR](https://github.com/ossf/osv-schema/compare). |\n'
+
+  md = '{0}{1}\n\n{2}\n{3}{4}'.format(
+    md[:table_start_index],
+    MARKDOWN_TABLE_MARKER_START,
+    table,
+    MARKDOWN_TABLE_MARKER_END,
+    md[table_end_index + len(MARKDOWN_TABLE_MARKER_END):]
+  )
+
+  open('docs/schema.md', 'w').write(md)
+
+
 update_json_schema()
+update_schema_md()
