e67e37559f0430350c0afbe33c0b2e85f1e5b734
Gareth Jones <jones258@gmail.com>
2024-10-21 16:10:22 +1300

    feat: support updating json schema file
    Signed-off-by: Gareth Jones <jones258@gmail.com>

--- scripts/update-ecosystems-lists.py
+++ scripts/update-ecosystems-lists.py
@@ -4,4 +4,28 @@ import json
 
 ecosystems: dict[str, str] = json.loads(open('ecosystems.json').read())
 
-# nothing to do for now ;)
+
+def update_json_schema():
+  """
+  Updates references to ecosystems defined in the OSV JSON schema
+  :return:
+  """
+  schema = json.loads(open('validation/schema.json').read())
+
+  names = ecosystems.keys()
+  pattern = ""
+
+  for name in names:
+    pattern += name.replace(".", "\\.")
+    pattern += "|"
+
+  # this is a special "ecosystem" name
+  pattern += "GIT"
+
+  schema['$defs']['ecosystemName']['enum'] = list(names)
+  schema['$defs']['ecosystemWithSuffix']['pattern'] = f'^({pattern})(:.+)?$'
+
+  open('validation/schema.json', 'w').write(json.dumps(schema, indent=2) + '\n')
+
+
+update_json_schema()
