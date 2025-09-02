c8fb2be67ca7935057a195b817ccc88fad38fdfa
Ori Zerah <ori.zerah@echohq.com>
2025-09-02 13:43:14 +0300

    add Echo to lint ecosystems
    Signed-off-by: Ori Zerah <ori.zerah@echohq.com>

--- tools/osv-linter/internal/pkgchecker/ecosystems.go
+++ tools/osv-linter/internal/pkgchecker/ecosystems.go
@@ -54,6 +54,8 @@ func ExistsInEcosystem(pkg string, ecosystem string) bool {
 		return existsInCrates(pkg)
 	case "Debian":
 		return true
+	case "Echo":
+		return true
 	case "GIT":
 		return true
 	case "GitHub Actions":
@@ -147,6 +149,8 @@ func VersionsExistInEcosystem(pkg string, versions []string, ecosystem string) e
 	case "crates.io":
 		return versionsExistInCrates(pkg, versions)
 	case "Debian":
+		return nil
+	case "Echo":
 		return nil
 	case "GIT":
 		return nil
